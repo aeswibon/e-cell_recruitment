4cf702c010cff42e861f8194bcf3f2a43475daa7
cp-Coder <cplusplusCoder.vnit@gmail.com>
2021-07-24 19:49:30 +0530

    Final

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -1,5 +1,12 @@
-function show() {
-    btn = document.querySelector('#share_sites');
-    if (btn.style.display == 'none') btn.style.display = 'flex';
-    else btn.style.display = 'none';
+function funAlert() {
+    var btn = document.querySelector('#share_box');
+    var share_btn = document.querySelector('#share');
+	
+    if (btn.style.display == 'none') {
+        btn.style.display = 'flex';
+        share_btn.src = "images/sharing_hover.png";
+    } else {
+        btn.style.display = 'none';
+        share_btn.src = "images/sharing.png";
+    }
 }
